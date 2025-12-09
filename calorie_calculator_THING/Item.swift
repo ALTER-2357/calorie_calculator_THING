1ca00da456207b3f831cf7fd6d99b7e95e7d3c69
ALTER-2357 <lewismills290@protonmail.com>
2025-12-09 11:33:32 +0000

    Initial Commit

---
+++ calorie_calculator_THING/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  calorie_calculator_THING
+//
+//  Created by lewis mills on 09/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ calorie_calculator_THING/calorie_calculator_THINGApp.swift
@@ -0,0 +1,32 @@
+//
+//  calorie_calculator_THINGApp.swift
+//  calorie_calculator_THING
+//
+//  Created by lewis mills on 09/12/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct calorie_calculator_THINGApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
